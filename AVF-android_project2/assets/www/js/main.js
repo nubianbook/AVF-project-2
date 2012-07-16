eae68f7a3d21d75a31e4a933d5dcc6b693a9c588
nubianbook <nubianbook@me.com>
2012-07-16 13:35:02 -0400

    I am updating my files for review

--- AVF-android_project2/assets/www/js/main.js
+++ AVF-android_project2/assets/www/js/main.js
@@ -1,91 +1,33 @@
+ // Wait for PhoneGap to load
+    //
+    document.addEventListener("deviceready", onDeviceReady, false);
 
-var watchID;
-var geo;    // for the geolocation object
-var map;    // for the google map object
-var mapMarker;  // the google map marker object
-
-// position options
-var MAXIMUM_AGE = 200; // miliseconds
-var TIMEOUT = 300000;
-var HIGHACCURACY = true;
-
-function getGeoLocation() {
-    try {
-        if( !! navigator.geolocation ) return navigator.geolocation;
-        else return undefined;
-    } catch(e) {
-        return undefined;
+    // PhoneGap is ready
+    //
+    function onDeviceReady() {
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
     }
-}
 
-function show_map(position) {
-    var lat = position.coords.latitude;
-    var lon = position.coords.longitude;
-    var latlng = new google.maps.LatLng(lat, lon);
-    
-    if(map) {
-        map.panTo(latlng);
-        mapMarker.setPosition(latlng);
-    } else {
-        var myOptions = {
-        zoom: 18,
-        center: latlng,
-            
-            // mapTypeID --
-            // ROADMAP displays the default road map view
-            // SATELLITE displays Google Earth satellite images
-            // HYBRID displays a mixture of normal and satellite views
-            // TERRAIN displays a physical map based on terrain information.
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-        map.setTilt(0); // turns off the annoying default 45-deg view
-        
-        mapMarker = new google.maps.Marker({
-                                           position: latlng,
-                                           title:"You are here."
-                                           });
-        mapMarker.setMap(map);
+    // onSuccess Geolocation
+    //
+    function onSuccess(position) {
+        var element = document.getElementById('geolocation');
+        element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+                            'Longitude: '          + position.coords.longitude             + '<br />' +
+                            'Altitude: '           + position.coords.altitude              + '<br />' +
+                            'Accuracy: '           + position.coords.accuracy              + '<br />' +
+                            'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
+                            'Heading: '            + position.coords.heading               + '<br />' +
+                            'Speed: '              + position.coords.speed                 + '<br />' +
+                            'Timestamp: '          + new Date(position.timestamp)          + '<br />';
     }
-}
 
-function geo_error(error) {
-    stopWatching();
-    switch(error.code) {
-        case error.TIMEOUT:
-            alert('Geolocation Timeout');
-            break;
-        case error.POSITION_UNAVAILABLE:
-            alert('Geolocation Position unavailable');
-            break;
-        case error.PERMISSION_DENIED:
-            alert('Geolocation Permission denied');
-            break;
-        default:
-            alert('Geolocation returned an unknown error code: ' + error.code);
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
     }
-}
-
-function stopWatching() {
-    if(watchID) geo.clearWatch(watchID);
-    watchID = null;
-}
-
-function startWatching() {
-    watchID = geo.watchPosition(show_map, geo_error, {
-                                enableHighAccuracy: HIGHACCURACY,
-                                maximumAge: MAXIMUM_AGE,
-                                timeout: TIMEOUT
-                                });
-}
-
-window.onload = function() {
-    if((geo = getGeoLocation())) {
-        startWatching();
-    } else {
-        alert('Geolocation not supported.')
-    }
-}
 
 // Wait for Cordova to load
 //
@@ -114,50 +56,4 @@ function showAlert() {
 }
 
 				
-				// The watch id references the current `watchAcceleration`
-var watchID = null;
-
-// Wait for PhoneGap to load
-//
-document.addEventListener("deviceready", onDeviceReady, false);
-
-// PhoneGap is ready
-//
-function onDeviceReady() {
-    startWatch();
-}
-
-// Start watching the acceleration
-//
-function startWatch() {
-    
-    // Update acceleration every 3 seconds
-    var options = { frequency: 3000 };
-    
-    watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
-}
-
-// Stop watching the acceleration
-//
-function stopWatch() {
-    if (watchID) {
-        navigator.accelerometer.clearWatch(watchID);
-        watchID = null;
-    }
-}
-
-// onSuccess: Get a snapshot of the current acceleration
-//
-function onSuccess(acceleration) {
-    var element = document.getElementById('accelerometer');
-    element.innerHTML = 'Acceleration X: ' + acceleration.x + '<br />' +
-    'Acceleration Y: ' + acceleration.y + '<br />' +
-    'Acceleration Z: ' + acceleration.z + '<br />' +
-    'Timestamp: '      + acceleration.timestamp + '<br />';
-}
-
-// onError: Failed to get the acceleration
-//
-function onError() {
-    alert('onError!');
-}
+				
